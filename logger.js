6c5260480c8b7d0a274b4529c65a93f0725628c6
stripoutlondon <info@stripoutlondon.co.uk>
2025-11-13 15:51:21 +0000

    Delete logger.js

--- logger.js
+++
@@ -1,6 +0,0 @@
-// utils/logger.js
-module.exports = {
-  info: (...msg) => console.log("[INFO]", ...msg),
-  warn: (...msg) => console.log("[WARN]", ...msg),
-  error: (...msg) => console.error("[ERROR]", ...msg)
-};
